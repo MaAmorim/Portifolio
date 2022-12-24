b1b555642bc8c0d98b34fc30e1c736e74a953b33
Matheus Amorim <matheus.amorim3110@hotmail.com>
2022-12-24 17:39:19 -0300

    add noreferrer on links

--- components/SocialMedia/index.js
+++ components/SocialMedia/index.js
@@ -5,11 +5,11 @@ import styles from './styles.module.scss'
 const SocialMedia = ()=> {
   return (
     <div className={styles.socialMedia}>
-      <a className={styles.link} href="https://www.linkedin.com/in/matheus-amorim-developer/" target="_blank" rel='noopener'>
+      <a className={styles.link} href="https://www.linkedin.com/in/matheus-amorim-developer/" target="_blank" rel='noreferrer'>
         <Image src="/images/socialMedia/linkedin-icon.svg" width={45} height={45} alt="linkedin logo"/>
         <span className={styles.linkName}>LinkedIn</span>
       </a>
-      <a className={styles.link} href="" target="_blank" rel='noopener'>
+      <a className={styles.link} href="" target="_blank" rel='noreferrer'>
         <Image src="/images/socialMedia/github-icon.svg" width={45} height={45} alt="github logo"/>
         <span className={styles.linkName}>GitHub</span>
       </a>
